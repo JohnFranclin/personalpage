827c787c59a1b580364376eb618a1774fe167db9
JohnFranclin <61628402+JohnFranclin@users.noreply.github.com>
2022-06-07 12:09:49 +0200

    me picture hovereffect + cleanup

--- src/pages/Me.jsx
+++ src/pages/Me.jsx
@@ -3,7 +3,8 @@ import Face from "../images/face.jpeg";
 
 export default function Me() {
     return (
-        <div className="transition-all delay-300 duration-300 flex flex-col w-auto min-h-screen min-w-screen bg-amber-200 pb-4">
+        <div
+            className="transition-all delay-300 duration-300 flex flex-col w-auto min-h-screen min-w-screen bg-amber-200 pb-4">
             <Navbar active={0}/>
             <div className="m-auto md:container md:mx-auto md:my-auto m-2">
                 <div className="grid md:grid-cols-2 gap-4">
@@ -14,7 +15,9 @@ export default function Me() {
                         <div className="text-2xl text-slate-500">Business Information Systems Student</div>
                     </div>
                     <div className="">
-                        <img src={Face} alt="..." className="rounded-full m-auto h-auto max-w-xs shadow-2xl transition-all"/>
+                        <img src={Face} alt="..."
+                             className="rounded-full m-auto h-auto max-w-xs shadow-xl transition-all duration-300
+                             hover:scale-[1.005] hover:shadow-2xl"/>
                     </div>
                 </div>
             </div>
