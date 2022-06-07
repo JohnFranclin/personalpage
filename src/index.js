2014a257eb8ac75d574d35dedc1b8d4473aecba6
JohnFranclin <61628402+JohnFranclin@users.noreply.github.com>
2022-06-07 11:50:01 +0200

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Coding from "./pages/Coding";
+import AboutMe from "./pages/AboutMe";
+import Contact from "./pages/Contact";
+import Me from "./pages/Me";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <div className="App">
+          <BrowserRouter>
+              <Routes>
+                  <Route exact path="/" element={<Me />} />
+                  <Route path="/aboutme" element={<AboutMe />}/>
+                  <Route path="/coding" element={<Coding />}/>
+                  <Route path="/contact" element={<Contact />} />
+              </Routes>
+          </BrowserRouter>
+      </div>
   </React.StrictMode>
 );
 
